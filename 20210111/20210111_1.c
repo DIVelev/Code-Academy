fbe13b3cab8040d1b1cc710bb4552042f053ce38
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-01-12 11:06:42 +0200

    Create 20210111_1.c

---
+++ 20210111/20210111_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 3;
+    int b = 5;
+    int rez = 3 & 5;
+    printf("%d \n",rez);
+    rez = 3 | 5;
+    printf("%d \n",rez);
+    rez = 3 ^ 5;
+    printf("%d \n",rez);
+}
