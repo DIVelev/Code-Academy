832b566d93aef5e63e37e4f9231043b0fcb57679
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-01-14 09:07:51 +0200

    Create 20210113_13.c

---
+++ 20210113/20210113_13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char a = 'a';
+    printf("%d\n",sizeof(a));
+    int b = 102;
+    printf("%d\n",sizeof(b));
+    long long c = 50000;
+    printf("%d\n",sizeof(c));
+    float d=6;
+    printf("%d\n",sizeof(d));
+    double e = -48.907;
+    printf("%d\n",sizeof(e));
+
+    return 0;
+}
