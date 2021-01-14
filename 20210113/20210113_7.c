39ccf97455f1e88a0e9fb29aa87b916988ae6f10
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-01-14 09:06:08 +0200

    Create 20210113_7.c

---
+++ 20210113/20210113_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    short int a = 24212;
+    printf("%d \n",a);
+    int b = -1357674;
+    printf("%d \n",b);
+    unsigned int c = 1357674;
+    printf("%u \n",c);
+    long int d = -1357674000;
+    printf("%ld \n",d);
+    unsigned long long e = 3657895000;
+    printf("%llu \n",e);
+
+}
