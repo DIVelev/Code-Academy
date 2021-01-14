4a3ab3e3ad626b4c3ac271a1a021ae8c1529af27
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-01-14 09:05:22 +0200

    Create 20210113_3.c

---
+++ 20210113/20210113_3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned char z = 0;
+    printf("%d \n",z);
+    signed char a = -10;
+    printf("%d \n",a);
+    z = z + a;
+    printf("%d \n",z);
+    return 0;
+}
