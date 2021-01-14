a1e65ea54b33e2e45c9c602094863976990f1605
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-01-14 09:05:58 +0200

    Create 20210113_6.c

---
+++ 20210113/20210113_6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = -127;
+    printf("%d \n",a);
+    unsigned char b = 255;
+    printf("%u \n",b);
+    unsigned int c = 6237498;
+    printf("%u \n",c);
+    unsigned int d = 4294967292;
+    printf("%u \n",d);
+    long long e = -9000000000000775845;
+    printf("%lld \n",e);
+
+}
