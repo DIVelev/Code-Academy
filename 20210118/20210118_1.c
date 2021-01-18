69be0276e99c4adf7f1057145be1937e2d2240e1
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-01-18 23:10:20 +0200

    Create 20210118_1.c

---
+++ 20210118/20210118_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 10;
+    int y = 10;
+    int true,false;
+
+    true = x==y;
+    false = x!=y;
+
+    printf("True: %d\n",true);
+    printf("False: %d\n",false);
+
+    return 0;
+}
